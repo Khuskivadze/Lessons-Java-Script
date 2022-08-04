54e5994ecffb44d4c6a8084fd2198a098be798ec
Tamar  khuskivadze <86792404+Khuskivadze@users.noreply.github.com>
2022-08-04 23:27:55 +0400

    new

---
+++ Day 1/index.js
@@ -0,0 +1,37 @@
+//არითმეტიკული ოპერატორები -  + - * /  %
+
+// console.log ("hello Java script") 
+// console.log (20+15)
+// console.log (20-15)
+// console.log (20*15)
+// console.log (20/15)
+// console.log (20%15)
+//არითმეტიკული ოპერატორები-------------------
+
+// ცვლადები  variable
+// var userAge = 100
+// console.log(userAge)
+
+// var userAge = 21
+// console.log(userAge)
+// userAge = 15
+// console.log(userAge)
+
+// მონაცემთა ტიპები
+// number - მთელი რიცხვიან ათწილადი (მაგ:1,2,3 0,5 0.3);
+//bigint -  მთელი რიცხვი ან ათწილადი (განსხვავება Num და Bიგინტს შორის -
+//-  არის ის რომ ყველა ტიპს აქვს საზღვარი რამდენი რიცხვის შენახვა შეუძლია.)
+//sttring - სიტყვა  აღინიშნება ბრჭყალებით "" ან ''
+//boolean -  ლოგიკური მონაცემთა ტიპი - მაგ : true(1) or false(0)
+//array - მასივი  -  ინახავს ბევრ ინფორმაციას, ინფორმაციის კრებული 
+//object -  მასივი - მონაცემთა კრებული 
+//nun , null, undefined - სიცარიელე. 
+
+var bool = true
+ 
+
+
+ 
+
+
+ 
